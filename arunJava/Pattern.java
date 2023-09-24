8481e0d1f9b45f3da38ab474d0f47bc40b8e70ab
Gurunanthan <gurunanthan.r.j@gmail.com>
2023-09-24 19:09:24 +0530

    renamed the text file ext

--- arunJava/Pattern.java
+++ arunJava/Pattern.java
@@ -1,23 +1,24 @@
 package arunJava;
+
 //    1
 //  1 2
 //1 2 3
 import java.util.*;
+
 public class Pattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=1;j<i;j++){
-                if(j>=n-i)
-                {
-                    System.out.print(j);
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 1; j < i; j++) {
+                    if (j >= n - i) {
+                        System.out.print(j);
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
-                else{
-                    System.out.print(" ");
-                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
